3d9ad23c753f2a8630e939f9c1eaba125f42c3ee
Cduartev <cayooduartevidal@gmail.com>
2026-02-26 02:35:42 -0300

    refactor: refatoracao da pagina pois o projeto pedia que o back-end seguisse boas praticas de clean code em ingles entao renomeei todos nos nomes das pastas para ingles para ficar correto com o desafio do projeto

---
+++ src/main/java/com/project/inventory/dto/production/ProductionRequest.java
@@ -0,0 +1,13 @@
+package com.project.inventory.dto.production;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record ProductionRequest(
+        @NotNull(message = "O ID do produto é obrigatório")
+        Long productId,
+
+        @NotNull(message = "A quantidade a ser produzida é obrigatória")
+        @Min(value = 1, message = "A quantidade produzida deve ser no mínimo 1")
+        Integer quantity
+) {}
